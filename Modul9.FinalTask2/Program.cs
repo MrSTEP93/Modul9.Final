1cf14bf202587967d8053e5a7fbbeb7a3c2619f9
Shatalov.AG <mrstep93@ya.ru>
2024-02-02 01:04:42 +0300

    ready steady go

---
+++ Modul9.FinalTask2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using static ConsoleHelper_50.Helper_50;
+
+namespace Modul9.FinalTask2
+{
+    public delegate void Sort(List<Student> list);
+
+    internal class Program
+    {
+        public static List<Student> studentList = new()
+        {
+            new Student { surname = "Орлова" },
+            new Student { surname = "Белов" },
+            new Student { surname = "Степаненко" },
+            new Student { surname = "Барахвин" },
+            new Student { surname = "Ремизова" }
+        };
+
+        public static event Sort SortAsc;
+        public static event Sort SortDesc;
+
+        static void Main(string[] args)
+        {
+            WriteLn("Hello World!");
+            WriteLn("Исходный список фамилий:");
+            Student.PrintArray(studentList);
+
+            bool flag = false;
+            while (flag == false)
+            {
+                WriteLn("Введите (1) для сортировки А-Я и (2) для сортировки по Я-А. (0) для выхода из программы", true);
+                byte.TryParse(ReadLn(), out byte act);
+                try
+                {
+                    switch (act)
+                    {
+                        case 1: 
+                            SortAsc?.Invoke(studentList);
+                            WriteLn("Новый порядок массива");
+                            Student.PrintArray(studentList);
+                            break;
+                        case 2:
+                            SortDesc?.Invoke(studentList);
+                            WriteLn("Новый порядок массива");
+                            Student.PrintArray(studentList);
+                            break;
+                        case 0:
+                            flag = true;
+                            break;
+                        default:
+                            throw new ArgumentException("Введенная команда некорректна");
+                    }
+                }
+                catch (Exception e)
+                {
+                    WriteLn(e.Message, ConsoleColor.Red);
+                }
+            }
+            WriteLn("Нажмите Enter, чтобы закрыть это окно");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Modul9.FinalTask2/Student.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using static ConsoleHelper_50.Helper_50;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul9.FinalTask2
+{
+    public class Student : IComparable<string>
+    {
+
+        public string surname;
+
+        public Student()
+        {
+            Program.SortAsc += SortAZ;
+            Program.SortDesc += SortZA;
+        }
+
+        public static void PrintArray(List<Student> list)
+        {
+            foreach (var man in list)
+            {
+                WriteLn("\t" + man.surname);
+            }
+            WriteLn("");
+        }
+
+        public int CompareTo(string other)
+        {
+            return surname.CompareTo(other);
+        }
+
+        public static void SortAZ(List<Student> list)
+        {
+            for (int i = 0; i < list.Count - 1; i++)
+            {
+                for (int j = i + 1; j < list.Count; j++)
+                {
+                    if (list[i].surname.CompareTo(list[j].surname) > 0)
+                    {
+                        Student temp = list[i];
+                        list[i] = list[j];
+                        list[j] = temp;
+                    }
+                }
+            }
+        }
+
+        public static void SortZA(List<Student> list)
+        {
+            SortAZ(list);
+            list.Reverse();
+        }
+
+    }
+}
